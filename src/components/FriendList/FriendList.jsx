39d26335f978ef413b83693aaa7d25716a538780
KseniaLobova <128805342+KseniaLobova@users.noreply.github.com>
2023-10-01 18:53:22 +0200

    complet

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+import css from '../FriendList/FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItem
+            key={id}
+            status={isOnline}
+            avatar={avatar}
+            name={name}
+          />
+        );
+      })}
+    </ul>
+  );
+};
